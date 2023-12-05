349faa7467d05640ef031a3fadef4bb4bf373a93
Lucasalbini-94 <lucasalbini_94@hotmail.com>
2023-12-05 01:05:51 -0300

    Primera-subida

---
+++ Test_OpenCV/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace Test_OpenCV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbxVideo = new System.Windows.Forms.PictureBox();
+            this.btnGrabar = new System.Windows.Forms.Button();
+            this.btnDetectarRostro = new System.Windows.Forms.Button();
+            this.tbxNombrePersona = new System.Windows.Forms.TextBox();
+            this.btnAgregarPersona = new System.Windows.Forms.Button();
+            this.btnTren = new System.Windows.Forms.Button();
+            this.btnReconocer = new System.Windows.Forms.Button();
+            this.pbxDetectado = new System.Windows.Forms.PictureBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.pbxComparador1 = new System.Windows.Forms.PictureBox();
+            this.pbxComparador2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxVideo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDetectado)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxComparador1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxComparador2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbxVideo
+            // 
+            this.pbxVideo.Location = new System.Drawing.Point(12, 12);
+            this.pbxVideo.Name = "pbxVideo";
+            this.pbxVideo.Size = new System.Drawing.Size(550, 408);
+            this.pbxVideo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxVideo.TabIndex = 0;
+            this.pbxVideo.TabStop = false;
+            // 
+            // btnGrabar
+            // 
+            this.btnGrabar.Location = new System.Drawing.Point(568, 12);
+            this.btnGrabar.Name = "btnGrabar";
+            this.btnGrabar.Size = new System.Drawing.Size(130, 23);
+            this.btnGrabar.TabIndex = 2;
+            this.btnGrabar.Text = "Grabar";
+            this.btnGrabar.UseVisualStyleBackColor = true;
+            this.btnGrabar.Click += new System.EventHandler(this.btnGrabar_Click);
+            // 
+            // btnDetectarRostro
+            // 
+            this.btnDetectarRostro.Location = new System.Drawing.Point(568, 41);
+            this.btnDetectarRostro.Name = "btnDetectarRostro";
+            this.btnDetectarRostro.Size = new System.Drawing.Size(130, 23);
+            this.btnDetectarRostro.TabIndex = 3;
+            this.btnDetectarRostro.Text = "Detectar rostro";
+            this.btnDetectarRostro.UseVisualStyleBackColor = true;
+            this.btnDetectarRostro.Click += new System.EventHandler(this.btnDetectarRostro_Click);
+            // 
+            // tbxNombrePersona
+            // 
+            this.tbxNombrePersona.Location = new System.Drawing.Point(568, 211);
+            this.tbxNombrePersona.Name = "tbxNombrePersona";
+            this.tbxNombrePersona.Size = new System.Drawing.Size(130, 20);
+            this.tbxNombrePersona.TabIndex = 4;
+            // 
+            // btnAgregarPersona
+            // 
+            this.btnAgregarPersona.Location = new System.Drawing.Point(568, 70);
+            this.btnAgregarPersona.Name = "btnAgregarPersona";
+            this.btnAgregarPersona.Size = new System.Drawing.Size(130, 23);
+            this.btnAgregarPersona.TabIndex = 5;
+            this.btnAgregarPersona.Text = "Agregar persona";
+            this.btnAgregarPersona.UseVisualStyleBackColor = true;
+            this.btnAgregarPersona.Click += new System.EventHandler(this.btnAgregarPersona_Click);
+            // 
+            // btnTren
+            // 
+            this.btnTren.Location = new System.Drawing.Point(567, 266);
+            this.btnTren.Name = "btnTren";
+            this.btnTren.Size = new System.Drawing.Size(130, 23);
+            this.btnTren.TabIndex = 6;
+            this.btnTren.Text = "Imagenes de tren";
+            this.btnTren.UseVisualStyleBackColor = true;
+            this.btnTren.Click += new System.EventHandler(this.btnTren_Click);
+            // 
+            // btnReconocer
+            // 
+            this.btnReconocer.Location = new System.Drawing.Point(568, 295);
+            this.btnReconocer.Name = "btnReconocer";
+            this.btnReconocer.Size = new System.Drawing.Size(130, 23);
+            this.btnReconocer.TabIndex = 7;
+            this.btnReconocer.Text = "Reconocer persona";
+            this.btnReconocer.UseVisualStyleBackColor = true;
+            // 
+            // pbxDetectado
+            // 
+            this.pbxDetectado.Location = new System.Drawing.Point(568, 99);
+            this.pbxDetectado.Name = "pbxDetectado";
+            this.pbxDetectado.Size = new System.Drawing.Size(130, 106);
+            this.pbxDetectado.TabIndex = 8;
+            this.pbxDetectado.TabStop = false;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(568, 237);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(129, 23);
+            this.btnGuardar.TabIndex = 9;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // pbxComparador1
+            // 
+            this.pbxComparador1.Location = new System.Drawing.Point(568, 324);
+            this.pbxComparador1.Name = "pbxComparador1";
+            this.pbxComparador1.Size = new System.Drawing.Size(58, 51);
+            this.pbxComparador1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxComparador1.TabIndex = 10;
+            this.pbxComparador1.TabStop = false;
+            // 
+            // pbxComparador2
+            // 
+            this.pbxComparador2.Location = new System.Drawing.Point(632, 324);
+            this.pbxComparador2.Name = "pbxComparador2";
+            this.pbxComparador2.Size = new System.Drawing.Size(66, 51);
+            this.pbxComparador2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxComparador2.TabIndex = 11;
+            this.pbxComparador2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 475);
+            this.Controls.Add(this.pbxComparador2);
+            this.Controls.Add(this.pbxComparador1);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.pbxDetectado);
+            this.Controls.Add(this.btnReconocer);
+            this.Controls.Add(this.btnTren);
+            this.Controls.Add(this.btnAgregarPersona);
+            this.Controls.Add(this.tbxNombrePersona);
+            this.Controls.Add(this.btnDetectarRostro);
+            this.Controls.Add(this.btnGrabar);
+            this.Controls.Add(this.pbxVideo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxVideo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDetectado)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxComparador1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxComparador2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbxVideo;
+        private System.Windows.Forms.Button btnGrabar;
+        private System.Windows.Forms.Button btnDetectarRostro;
+        private System.Windows.Forms.TextBox tbxNombrePersona;
+        private System.Windows.Forms.Button btnAgregarPersona;
+        private System.Windows.Forms.Button btnTren;
+        private System.Windows.Forms.Button btnReconocer;
+        private System.Windows.Forms.PictureBox pbxDetectado;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.PictureBox pbxComparador1;
+        private System.Windows.Forms.PictureBox pbxComparador2;
+    }
+}
+
---
+++ Test_OpenCV/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Face;
+using Emgu.CV.CvEnum;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Test_OpenCV
+{
+    public partial class Form1 : Form
+    {
+        #region Variables
+        private VideoCapture video = null;
+        private Image<Bgr, Byte> cuadroActual = null;
+        Mat cuadro = new Mat();
+        private bool habilitarDetectorRostro = false;
+        private CascadeClassifier clasificadorRostro = new CascadeClassifier(@"C:\opencv\build\etc\haarcascades\haarcascade_frontalface_alt.xml");
+        Image<Bgr, Byte> resultadoRostro = null;
+        List<Image<Gray, Byte>> rostrosEntrenados = new List<Image<Gray, byte>>();
+        List<int> etiquetasPersona = new List<int>();
+        private bool habilitarGuardarImagen = true;
+        private static bool entrenando = false;
+        EigenFaceRecognizer reconocedor;
+        List<string> nombresPersona = new List<string>();
+
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGrabar_Click(object sender, EventArgs e)
+        {
+            video = new VideoCapture();
+            video.ImageGrabbed += ProcesarCuadro;
+            video.Start();
+        }
+
+        private void ProcesarCuadro(object sender, EventArgs e)
+        {
+            // Paso 1: Captura de video
+            video.Retrieve(cuadro, 0);
+            cuadroActual = cuadro.ToImage<Bgr, Byte>().Resize(pbxVideo.Width, pbxVideo.Height, Inter.Cubic);
+            // Paso 2: Detectar rostro
+            if (habilitarDetectorRostro)
+            {
+                // Convertir cuadro a gris
+                Mat imagenGris = new Mat();
+                CvInvoke.CvtColor(cuadroActual, imagenGris, ColorConversion.Bgr2Gray);
+                // Mejorar imágen para obtener mejores resultados
+                CvInvoke.EqualizeHist(imagenGris, imagenGris);
+
+                Rectangle[] rostros = clasificadorRostro.DetectMultiScale(imagenGris, 1.1, 3, Size.Empty, Size.Empty);
+
+                if (rostros.Length > 0)
+                {
+
+                    foreach (var rostro in rostros)
+                    {
+                        CvInvoke.Rectangle(cuadroActual, rostro, new Bgr(Color.Red).MCvScalar, 2);
+
+                        // Paso 3: Agregar persona
+                        // Asignar rostro al picture box de persona
+                        Image<Bgr, Byte> resultadoImagen = cuadroActual.Convert<Bgr, Byte>();
+                        resultadoImagen.ROI = rostro;
+                        pbxDetectado.SizeMode = PictureBoxSizeMode.StretchImage;
+                        pbxDetectado.Image = resultadoImagen.Bitmap;
+
+                        if (habilitarGuardarImagen)
+                        {
+                            // Crear directorio si no existe
+                            string ruta = Directory.GetCurrentDirectory() + @"\ImagenesEntrenamiento";
+                            if (!Directory.Exists(ruta))
+                                Directory.CreateDirectory(ruta);
+
+                            // Guardar 10 imágenes con retraso de segundo cada una
+                            Task.Factory.StartNew(() =>
+                            {
+                                for (int x = 0; x < 10; x++)
+                                {
+                                    // Redimencionar imagen y guardar resultado
+                                    resultadoImagen.Resize(200, 200, Inter.Cubic).Save(ruta + @"\" + tbxNombrePersona.Text + "_" + DateTime.Now.ToString("dd-mm-yyyy-hh-mm-ss") + ".jpg");
+                                    Thread.Sleep(1000);
+                                }
+                            });
+                        }
+                        habilitarGuardarImagen = false;
+
+                        if (btnAgregarPersona.InvokeRequired)
+                        {
+                            btnAgregarPersona.Invoke(new ThreadStart(delegate
+                            {
+                                btnAgregarPersona.Enabled = true;
+                            }));
+                        }
+
+                        // Paso 5: Reconocer rostros
+                        if (entrenando)
+                        {
+                            Image<Gray, Byte> rostroGrisResultado = resultadoImagen.Convert<Gray, Byte>().Resize(200, 200, Inter.Cubic);
+                            CvInvoke.EqualizeHist(rostroGrisResultado, rostroGrisResultado);
+                            var resultado = reconocedor.Predict(rostroGrisResultado);
+                            pbxComparador1.Image = rostroGrisResultado.Bitmap;
+                            if (resultado.Label >= 0 && resultado.Label > rostrosEntrenados.Count)
+                                pbxComparador2.Image = rostrosEntrenados[resultado.Label].Bitmap;
+                            else
+                                Debug.WriteLine("El índice está fuera de rango:" + resultado.Label);
+                            Debug.WriteLine(resultado.Label + ". ", resultado.Distance);
+                            // Se encuentra un resultado de rostro
+                            if (resultado.Label != -1 && resultado.Distance < 2000)
+                            {
+                                CvInvoke.PutText(cuadroActual, nombresPersona[resultado.Label], new Point(rostro.X - 2, rostro.Y - 2),
+                                    FontFace.HersheyComplex, 1.0, new Bgr(Color.Orange).MCvScalar);
+                                CvInvoke.Rectangle(cuadroActual, rostro, new Bgr(Color.Green).MCvScalar, 2);
+                            }
+                            // No se encuentra un resultado de rostro
+                            else
+                            {
+                                CvInvoke.PutText(cuadroActual, "Desconocido", new Point(rostro.X - 2, rostro.Y - 2),
+                                    FontFace.HersheyComplex, 1.0, new Bgr(Color.Orange).MCvScalar);
+                            }
+                        }
+
+                    }
+                }
+
+            }
+
+            //Renderizar el video en el cuadro de imágen
+            pbxVideo.Image = cuadroActual.Bitmap;
+
+        }
+
+        private void btnDetectarRostro_Click(object sender, EventArgs e)
+        {
+            habilitarDetectorRostro = true;
+        }
+
+        private void btnAgregarPersona_Click(object sender, EventArgs e)
+        {
+            btnGuardar.Enabled = true;
+            btnAgregarPersona.Enabled = false;
+            habilitarGuardarImagen = true;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            btnGuardar.Enabled = false;
+            btnAgregarPersona.Enabled = true;
+            habilitarGuardarImagen = false;
+        }
+
+        private void btnTren_Click(object sender, EventArgs e)
+        {
+            EntrenarImagenesDeDirectorio();
+        }
+
+        //Paso 4: Entrenar imágenes
+        private bool EntrenarImagenesDeDirectorio()
+        {
+            int contadorImagenes = 0;
+            double limite = -1;
+            rostrosEntrenados.Clear();
+            etiquetasPersona.Clear();
+            nombresPersona.Clear();
+
+            try
+            {
+                string ruta = Directory.GetCurrentDirectory() + @"\ImagenesEntrenamiento\";
+                string[] archivos = Directory.GetFiles(ruta, "*.jpg", SearchOption.AllDirectories);
+
+                foreach (var archivo in archivos)
+                {
+                    Image<Gray, byte> imagenEntrenamiento = new Image<Gray, byte>(archivo).Resize(200, 200, Inter.Cubic);
+                    CvInvoke.EqualizeHist(imagenEntrenamiento, imagenEntrenamiento);
+                    rostrosEntrenados.Add(imagenEntrenamiento);
+                    etiquetasPersona.Add(contadorImagenes);
+                    string nombre = archivo.Split('\\').Last().Split('_')[0];
+                    nombresPersona.Add(nombre);
+                    contadorImagenes++;
+                }
+
+                if (rostrosEntrenados.Count > 0)
+                {
+                    reconocedor = new EigenFaceRecognizer(contadorImagenes, limite);
+                    reconocedor.Train(new Emgu.CV.Util.VectorOfMat(rostrosEntrenados.Select(img => img.Mat).ToArray()),
+                        new Emgu.CV.Util.VectorOfInt(etiquetasPersona.ToArray()));
+
+
+                    entrenando = true;
+                    Debug.WriteLine(contadorImagenes);
+                    Debug.WriteLine(entrenando);
+
+                    return entrenando = true;
+                }
+                else
+                {
+                    entrenando = false;
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                entrenando = false;
+                MessageBox.Show("Error al entrenar imágenes: " + ex.Message);
+                return entrenando = false;
+            }
+        }
+    }
+}
